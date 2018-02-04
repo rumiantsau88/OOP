a8f682d55556c92d073de219abd3206377af273e
Uladzimir Rumiantsau <rumiantsau88@gmail.com>
2018-02-04 15:48:37 +0300

    Add projects

--- TaskFromMiau/Program.cs
+++ TaskFromMiau/Program.cs
@@ -8,15 +8,8 @@ namespace TaskFromMiau
 {
 	class Program
 	{
-		static public void Main(string[] args)
+		static void Main(string[] args)
 		{
-			Console.WriteLine("How many characters you want to get in password?");
-			GetPasswordClass pass = new GetPasswordClass(int.Parse(Console.ReadLine()));
-			Console.WriteLine(pass.Password);
-			Console.ReadKey();
-
-			
-
 		}
 	}
 }
