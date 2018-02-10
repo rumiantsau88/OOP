0c1609331347a3ec23dbd2b9c4f9fc5e5ca0d77a
Uladzimir Rumiantsau <rumiantsau88@gmail.com>
2018-02-10 22:36:08 +0300

    printer project

---
+++ Printer/NewPrinter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printer
+{
+    class NewPrinter:Printer
+    {
+        
+        public void PrintScreen(string val)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(val);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Printer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string val = "blablabla";
+            Printer inst = new Printer();
+            inst.PrintScreen(val);
+            NewPrinter inst2 = new NewPrinter();
+            inst2.PrintScreen(val);
+            Printer up = inst2 as Printer;
+            up.PrintScreen(val);
+            NewPrinter down = up as NewPrinter;
+            down.PrintScreen(val);
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}
