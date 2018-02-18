63bc9cc47735c459513a0c56de20d9c38d28c0bf
Uladzimir Rumiantsau <rumiantsau88@gmail.com>
2018-02-18 14:20:31 +0300

    Classroom

---
+++ ClassRoom/ClassRoom.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class ClassRoom
+    {
+        Pupil [] pupils = new Pupil[4];
+
+        public ClassRoom(Pupil p1,Pupil p2)
+        {
+            pupils[0] = p1;
+            pupils[1] = p2;
+            pupils[2] = GetPupil();
+            pupils[3] = GetPupil();
+        }
+
+        public ClassRoom(Pupil p1,Pupil p2,Pupil p3)
+        {
+            pupils[0] = p1;
+            pupils[1] = p2;
+            pupils[2] = p3;
+            pupils[3] = GetPupil();
+        }
+
+        Random rand = new Random();
+        public Pupil GetPupil()
+        {
+            int i = rand.Next(1, 4);
+            if(i==1)
+            {
+                return new ExcellentPupil();
+            }
+            if (true)
+            {
+
+            }
+        }
+
+    }
+}
---
+++ ClassRoom/Pupil.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class Pupil
+    {
+        public virtual void Write()    
+        {
+            Console.WriteLine("Pupil.Write()");
+        }
+
+        public virtual void Read()
+        {
+            Console.WriteLine("Pupil.Read()");
+        }
+
+        public virtual void Relax()
+        {
+            Console.WriteLine("Pupil.Relax");
+        }
+
+        public virtual void Study()
+        {
+            Console.WriteLine("Pupil.Study()");
+        }
+
+        //**********************************************************************************************************************
+
+
+
+
+        class ExcellentPupil:Pupil
+        {
+            public ExcellentPupil()
+            {
+
+            }
+            public override void Read()
+            {
+                Console.WriteLine("ExclellentPupil.Read()");
+            }
+
+            public override void Relax()
+            {
+                Console.WriteLine("ExcellentPupil.Relax()");
+            }
+
+            public override void Study()
+            {
+                Console.WriteLine("ExcellentPupil.Study()");
+            }
+
+            public override void Write()
+            {
+                Console.WriteLine("ExcellentPupil.Write()");
+            }
+        }
+
+
+
+
+        class GoodPupil:Pupil
+        {
+            public override void Read()
+            {
+                Console.WriteLine("GoodPupil.Read()");
+            }
+
+            public override void Relax()
+            {
+                Console.WriteLine("GoodPupil.Relax()");
+            }
+
+            public override void Study()
+            {
+                Console.WriteLine("GoodPupil.Study()");
+            }
+
+            public override void Write()
+            {
+                Console.WriteLine("GoodPupil.Write()");
+            }
+        }
+
+
+
+
+        class BadPupil:Pupil
+        {
+            public override void Read()
+            {
+                Console.WriteLine("BadPupil.Read()");
+            }
+
+            public override void Relax()
+            {
+                Console.WriteLine("BadPupil.Relax()");
+            }
+
+            public override void Study()
+            {
+                Console.WriteLine("BadPupil.Study()");
+            }
+
+            public override void Write()
+            {
+                Console.WriteLine("BadPupil.Write()");
+            }
+        }
+
+    }
+}
